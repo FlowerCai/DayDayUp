7da0985be27a083cac4b6ea44a12a6059115c9bb
caishifang <fangcsyuan79@163.com>
2022-10-26 19:47:12 +0800

    finish leetcode 20

--- LeetCode/leetcode20.js
+++ LeetCode/leetcode20.js
@@ -1,3 +1,50 @@
+// 有漏洞的方案一
 var isValid = function(s) {
-  
+  const sign = {'(': 0, '{': 0, '[':0}
+  const dic = {')':'(', ']':'[', '}':'{'}
+  let len = s.length
+  if(len%2 == 1){
+    return false
+  }
+  for(let i=0; i<len; i++) {
+    let cur = s[i]
+    if(Object.keys(sign).includes(cur)){
+      sign[cur] ++ 
+    }else if(Object.keys(dic).includes(cur)){
+      sign[dic[cur]] --
+      if(sign[dic[cur]] < 0){
+        return false
+      }
+    }
+  }
+  let values = Object.values(sign).filter(v => v!= 0)
+  if(values.length != 0){
+    return false
+  }else{
+    return true
+  }
 };
+
+var isValid2 = function(s) {
+  const map = {
+    '(': ')',
+    '{': '}',
+    '[': ']',
+  }
+
+  let stack = []
+  for(let key of s){
+    if(Object.keys(map).includes(key)){
+      stack.push(key)
+    }else {
+      if(key == map[stack.pop()]){
+        continue
+      }else{
+        return false
+      }
+    }
+  }
+  return stack.length == 0 ? true : false
+
+}; 
+console.log(isValid2("()"))
