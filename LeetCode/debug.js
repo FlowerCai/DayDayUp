0fc0626d03d84a61afd3a5ebef5013b0f9bd5e9f
caishifang <fangcsyuan79@163.com>
2022-11-02 22:00:20 +0800

    finish leetcode 45

--- LeetCode/debug.js
+++ LeetCode/debug.js
@@ -1,3 +1,16 @@
-const a = [1, 2, 3]
-const b = Array.from(a, (x) => x ** 2)
-let cdnjhcd = 2
+var jump = function (nums) {
+  let len = nums.length
+  let end = 0
+  let maxPosition = 0
+  let steps = 0
+  for (let i = 0; i < len - 1; i++) {
+    maxPosition = Math.max(maxPosition, i + nums[i])
+    if (i == end) {
+      end = maxPosition
+      steps++
+    }
+  }
+  return steps
+}
+
+console.log(jump([2, 3, 1, 1, 4]))
